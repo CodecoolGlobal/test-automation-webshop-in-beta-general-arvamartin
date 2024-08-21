679f9a04028df5fbc1f91bab3d3432641a29a72e
adesz0112 <meszaros.adam94@gmail.com>
2024-08-21 11:39:12 +0200

    Created BasePage and LoginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,9 +1,28 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class LoginPage extends BasePage {
 
+    @FindBy (id="user-name")
+    public WebElement usernameField;
+    @FindBy(id = "password")
+    public WebElement passwordField;
+    @FindBy (id = "login-button")
+    public WebElement loginButton;
 
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 
+    public void enterUserNameAndPassword(String username, String password){
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+    }
 
+    public void clickToLogin(){
+        loginButton.click();
+    }
 }
